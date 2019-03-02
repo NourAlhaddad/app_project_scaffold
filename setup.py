14de8c18975a9ae511a5f54013a549822b662e99
Nour Alhaddad <nour609@hotmail.com>
2019-03-02 16:30:00 -0500

    Add setup.py for packaging

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
             "scaffold=create_project:main",
         ],
     },
-    author="Your Name",
+    author="Nour Alhaddad",
     description="Generate project folders and files from a structure document",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/app_project_scaffold",
+    url="https://github.com/NourAlhaddad/app_project_scaffold.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
